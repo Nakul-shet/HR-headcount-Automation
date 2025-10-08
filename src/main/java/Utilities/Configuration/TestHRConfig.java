426c9f612900434795fbf7cd817ee2d8dd016dd0
Nakul Shet <nakulnshet1@gmail.com>
2025-10-08 10:16:41 +0530

    Configuration updates

--- src/main/java/Utilities/Configuration/TestHRConfig.java
+++ src/main/java/Utilities/Configuration/TestHRConfig.java
@@ -12,12 +12,12 @@ public class TestHRConfig implements AppConfig {
     public String getPracticeTableName() { return "New Org Headcount"; }
     public String getOrgPractice() { return "practice"; }
     public String getOrgOperations() { return "operations"; }
-    public String getSenderId() { return "kmk@teksystems.com"; }
+    public String getSenderId() { return "TGSHRIndiaOps@teksystems.com"; }
     public String getSenderPassword() { return "****"; }
     public String getSharepointLinkPractice() { return "https://allegiscloud.sharepoint.com/...practice"; }
     public String getSharepointLinkOperations() { return "https://allegiscloud.sharepoint.com/...operations"; }
     public String[] getRecipients() { return new String[]{
-            "nshet@teksystems.com"
+            "vsada@teksystems.com"
     }; }
     public String spot(){return "spot";}
     public String distinguished(){return "distinguished";}
